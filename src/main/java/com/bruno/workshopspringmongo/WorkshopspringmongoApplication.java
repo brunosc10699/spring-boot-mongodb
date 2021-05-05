e5b202b1d93ecbf5ed8c2fd7fe9d3911f3cfc29d
Bruno <brunosc10699@gmail.com>
2021-05-04 23:38:38 -0300

    Database Instantiation

--- src/main/java/com/bruno/workshopspringmongo/WorkshopspringmongoApplication.java
+++ src/main/java/com/bruno/workshopspringmongo/WorkshopspringmongoApplication.java
@@ -1,5 +1,6 @@
 package com.bruno.workshopspringmongo;
 
+import com.bruno.workshopspringmongo.config.Instantiation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkshopspringmongoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WorkshopspringmongoApplication.class, args);
 	}
 
