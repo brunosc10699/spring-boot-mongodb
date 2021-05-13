cf6a83647e57e85cbbb9bab580e7b25be331fa41
Bruno <brunosc10699@gmail.com>
2021-05-13 00:13:12 -0300

    Insert User endpoint

--- src/main/java/com/bruno/workshopspringmongo/resources/UserResource.java
+++ src/main/java/com/bruno/workshopspringmongo/resources/UserResource.java
@@ -7,7 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.bruno.workshopspringmongo.domain.User;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,13 +22,24 @@ public class UserResource {
 
     @RequestMapping(method= RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll(){
-        List<UserDTO> list = userService.findAll().stream().map(UserDTO::new).collect(Collectors.toList());
-        return ResponseEntity.ok().body(list);
+        return ResponseEntity.ok(userService.findAll());
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
-        UserDTO userDTO = new UserDTO(userService.findById(id));
-        return ResponseEntity.ok().body(userDTO);
+        return ResponseEntity.ok(userService.findById(id));
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO){
+        User user = userService.fromDTO(userDTO);
+        userService.insert(user);
+        URI uri = ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(user.getId())
+                    .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
--- src/main/java/com/bruno/workshopspringmongo/services/UserService.java
+++ src/main/java/com/bruno/workshopspringmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.bruno.workshopspringmongo.services;
 
 import com.bruno.workshopspringmongo.domain.User;
+import com.bruno.workshopspringmongo.dto.UserDTO;
 import com.bruno.workshopspringmongo.repositories.UserRepository;
 import com.bruno.workshopspringmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -14,15 +16,23 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAll(){
-        return userRepository.findAll();
+    public List<UserDTO> findAll(){
+        return userRepository.findAll().stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
     }
 
-    public User findById(String id){
+    public UserDTO findById(String id){
         User user = userRepository.findById(id).orElse(null);
         if (user == null){
             throw new ObjectNotFoundException("Objeto não encontrado!");
         }
-        return user;
+        return new UserDTO(user);
+    }
+
+    public void insert(User user){
+        userRepository.save(user);
+    }
+
+    public User fromDTO(UserDTO userDTO){
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
