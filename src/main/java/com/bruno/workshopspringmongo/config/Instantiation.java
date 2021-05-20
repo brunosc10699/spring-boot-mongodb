e6b00909c39e3180b25c70cdca0d1dad21c4e5b0
Bruno <brunosc10699@gmail.com>
2021-05-20 11:14:36 -0300

    Entity Post created with nested User

--- src/main/java/com/bruno/workshopspringmongo/config/Instantiation.java
+++ src/main/java/com/bruno/workshopspringmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.bruno.workshopspringmongo.config;
 
+import com.bruno.workshopspringmongo.domain.Post;
 import com.bruno.workshopspringmongo.domain.User;
+import com.bruno.workshopspringmongo.repositories.PostRepository;
 import com.bruno.workshopspringmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,8 +18,14 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-YYYY");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
 
@@ -24,5 +34,27 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob", "bob@gmail.com");
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        postRepository.deleteAll();
+
+        Post post1 = new Post(
+                null,
+                sdf.parse("21-03-2018"),
+                "Partiu viagem",
+                "Vou viajar para São Paulo. Abraços!",
+                maria);
+
+        Post post2 = new Post(
+                null,
+                sdf.parse("23-03-2018"),
+                "Bom dia",
+                "Acordei feliz hoje!",
+                maria
+                );
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+
+
     }
 }
